0fd295d2ebd11b9ef59b658b386d77e6502e3a15
Lorenzo Hiver <lorenzohiver1@gmail.com>
2025-01-21 19:32:02 +0100

    feature(app): create useGetRace hook

--- app/app/hooks/index.ts
+++ app/app/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useRockets } from './useRockets';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useSubscriptionHook } from './useSubscription';
 export { default as useRace } from './useRace';
+export { default as useGetRace } from './useGetRace';
